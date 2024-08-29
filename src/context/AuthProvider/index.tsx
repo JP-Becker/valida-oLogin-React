ab035b3717cac8140825f67bf0d4b92532580fbb
JP-Becker <jpbecker20@gmail.com>
2024-08-29 11:29:54 -0300

    implementada funcionalidade de salvar usuário no localStorage

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -1,11 +1,19 @@
-import React, {createContext, useState} from "react";
+import React, {createContext, useEffect, useState} from "react";
 import { IAuthProvider, IContext, IUser } from "./types";
-import { LoginRequest } from "./util";
+import { getUserLocalStorage, LoginRequest, setUserLocalStorage } from "./util";
 
 export const AuthContext = createContext<IContext>({} as IContext);
 
 export const AuthProvider = ({ children }: IAuthProvider) => {
     const [user, setUser] = useState<IUser | null>();
+
+    useEffect(() => {
+        const user = getUserLocalStorage();
+
+        if (user) {
+            setUser(user);
+        }
+    }, []);
     
     async function authenticate (email: string, password: string) {
         const response = await LoginRequest(email, password); // faz a requisição a API passando o email e senha
@@ -13,10 +21,14 @@ export const AuthProvider = ({ children }: IAuthProvider) => {
         const payload = {token: response.token, email};
 
         setUser(payload); // carrega o usuário com o email e o token
+        setUserLocalStorage(payload);
+        // seta tanto no BD quanto no localStorage
     }
 
     function logout () {
         setUser(null); // zera os dados do usuário (desloga)
+        setUserLocalStorage(null);
+        // seta tanto no BD quanto no localStorage
     }
     
     return (
--- src/context/AuthProvider/util.ts
+++ src/context/AuthProvider/util.ts
@@ -1,4 +1,17 @@
 import { Api } from "../services/api";
+import { IUser } from "./types";
+
+export function setUserLocalStorage (user: IUser | null) { // função para armazenar o usuario no localStorage do browser
+    localStorage.setItem('u', JSON.stringify(user));// setando o item do local storage, sem medidas de segurança
+    // localStorage sempre armazena em JSON, então tem q converter a string
+}
+
+export function getUserLocalStorage () {
+    const json = localStorage.getItem('u'); //guardando o item buscado do localStorage (salvo em formato JSON)
+
+    return json ? JSON.parse(json) ?? null : null; //verificando se o item de fato existe no localStorage. se sim, retorna ele (user), senao retorna nulo
+}
+
 
 export async function LoginRequest (email: string, password: string) {
     try {
